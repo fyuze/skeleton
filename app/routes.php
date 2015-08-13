667d91f73dedf96df2c6259e366144be7cb9d587
0xMatt <matthew.javelet@gmail.com>
2015-08-12 20:02:44 -0500

    Drop symfony router

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,8 @@
 <?php
 // app/routing.php
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
 
-$collection = new RouteCollection();
-$collection->add('index', new Route('/', [
-    '_controller' => 'App\Http\Controllers\HomeController::indexAction'
-]));
-$collection->add('hello', new Route('/hello/{name}', [
-    '_controller' => 'App\Http\Controllers\HomeController::testAction',
-]));
-return $collection;
+use Fyuze\Http\Response;
+
+$router->get('', 'index', function() {
+   return new Response('Welcome to Fyuze!');
+});
--- src/App/Tests/BaseTest.php
+++ src/App/Tests/BaseTest.php
@@ -13,17 +13,6 @@ class BaseTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('Fyuze\Http\Response', $response);
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('Welcome to Fyuze!', $response->getContent());
-    }
-
-    public function testSecondaryRoute()
-    {
-        $path = realpath(__DIR__ . '/../../../');
-        $app = new Web($path);
-        $response = $app->boot($app->getContainer()->make('Fyuze\Http\Request')->create('/hello/bob'));
-
-        $this->assertInstanceOf('Fyuze\Http\Response', $response);
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('Hello, bob!', $response->getContent());
+        $this->assertEquals('Welcome to Fyuze!', $response->getBody());
     }
 }
