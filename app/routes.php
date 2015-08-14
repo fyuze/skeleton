933bf4dd4e2f07853eab292de1293a09c4030cfb
0xMatt <matthew.javelet@gmail.com>
2015-08-14 12:18:27 -0500

    unnecessary comment

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,4 @@
 <?php
-// app/routing.php
 
 $router->get('', 'index', 'App\Http\Controllers\HomeController@indexAction');
 $router->get('/hello/{name}', 'hello', 'App\Http\Controllers\HomeController@helloAction');
